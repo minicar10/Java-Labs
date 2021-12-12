1ce44be2cf14c020ef31f9fa9a3a92d9c39b420d
Zachariah Joseph <zachalexjoseph@yahoo.com>
2021-12-11 19:09:00 -0600

    ericklookinglikeasnack

--- ChristmasFun/Graveyard.java
+++ ChristmasFun/Graveyard.java
@@ -6,43 +6,45 @@ import java.util.*;
 public class Graveyard {
     public static void main (String[] args) throws FileNotFoundException
     {
-        Scanner sc = new Scanner(new File("ChristmasFun\\data\\graveyard.dat"));
-        Map<Character, Integer> letters = new HashMap<>();
+       Scanner scanner = new Scanner(new File("ChristmasFun\\data\\graveyard.dat"));
 
-        while (sc.hasNextLine()){
-            String line = sc.nextLine();
-            line = line.substring(1, line.length()-1).toUpperCase();
+       Map<Character, Integer> letters = new HashMap<>();
+       String buffer = "";
 
-            char[] characters = line.toCharArray();
+       while(scanner.hasNextLine()) {
+           char[] temp = scanner.nextLine().toCharArray();
 
-            for(char character: characters){
-                if(character != ' '){
-               if(letters.containsKey(character)){
-                   letters.put(character, letters.get(character)+1);
-               } else { 
-                   letters.put(character, 1);
+           for(char character: temp){
+               if(isAsciiLetter(character)){
+                   buffer += String.valueOf(character);
                }
-            }
-            }
+           }
+       }
 
-            char mostPrevalent = '?';
-            int maxOccurrences = Integer.MIN_VALUE;
+       buffer = buffer.toUpperCase();
 
-            for(Map.Entry<Character, Integer> entry : letters.entrySet()){
-                if(entry.getValue() > maxOccurrences){
-                    mostPrevalent = entry.getKey();
-                    maxOccurrences = entry.getValue();
+       for(char character: buffer.toCharArray()){
+           if(letters.get(character) == null){
+               letters.put(character, 1);
+           } else { 
+               letters.put(character, letters.get(character).intValue() + 1);
+           }
+       }
+
+            List<Map.Entry<Character, Integer>> sortedValueList = new LinkedList<>(letters.entrySet());
+
+            Collections.sort(sortedValueList, (one, two) -> -(one.getValue().compareTo(two.getValue())));                
+            
+            for(Map.Entry<Character, Integer> entry: sortedValueList){
+                for(int i = 0; i < entry.getValue(); i++){
+                    System.out.print(entry.getKey() + " ");
                 }
+                System.out.println();
             }
+    }
 
-            for(int i = 0; i < maxOccurrences; i++){
-                System.out.print(mostPrevalent + " ");
-            }
-
-            System.out.println();
-
-            letters.clear();
-        }
+    static boolean isAsciiLetter (Character c){
+        return c.hashCode() >= 65 &&  c.hashCode() <= 122;
     }
     }
 
